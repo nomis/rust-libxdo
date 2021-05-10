1268f90556d6d33af55b217e75a86f84894b2b22
Philipp Keck <git@philippkeck.de>
2021-05-10 21:51:26 +0200

    Implement Send+Sync for XDo
    Fixes #4 
    
    The underlying C library (xdolib) should be thread-safe:
    * The `xdo_t` struct contents do not change when `xdo_...` functions are invoked.
    * Some of the `xdo_...` functions have internal state that can get invalidated by other threads. This mostly pertains to the _root_ window (per screen) or the screen handle itself, which hopefully is pretty stable. Should the screen configuration change while an xdolib command is ongoing, it would return an error (e.g. BadWindow), but this doesn't make the library `unsafe` in the Rust sense.
    
    It's worth noting that some applications may still want to use a `Mutex` when executing XDo commands, because they can interfere with each other (e.g. pressing key combinations or moving the mouse and then clicking aren't atomic operations). The library won't fail when executing them concurrently, but they may not lead to the desired outcome. xdolib currently does _not_ support Xlib's built-in `XLockDisplay` mechanism, and it doesn't seem necessary/useful at this point.

--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,9 @@ pub struct XDo {
     handle: *mut sys::xdo_t,
 }
 
+unsafe impl Send for XDo {}
+unsafe impl Sync for XDo {}
+
 /// An error that can happen when trying to create an `XDo` instance.
 #[derive(Debug)]
 pub enum CreationError {
